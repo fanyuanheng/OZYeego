63a42870bac8aab4ca1f1ec8566300cc1f605798
Yuan Heng Fan <fanyuanheng@gmail.com>
2013-04-27 23:51:46 +1000

    refactored parse query method

--- js/ozyeego.js
+++ js/ozyeego.js
@@ -1,9 +1,38 @@
-function initMenuPanel() {
+var initMenuPanel = function() {
   var menuPanelTemplate = Mustache.to_html($('#menu-panel-template').html());
   _.each(['#products', '#about-us'], function(sel){
     $(sel).prepend(menuPanelTemplate);
   });
   $('#products').trigger("create");
+}
+
+var queryProducts = function(callback) {
+  $.mobile.showPageLoadingMsg();
+
+  var query = new Parse.Query(Parse.Object.extend("Product"));
+  query.equalTo("supplier", "ChemistWarehouse");
+  query.find({
+    success: function(results) {
+      var Products = new ProductList(_.map(results, function(result) {
+        return new Product({
+          name : result._serverData.name,
+          image_url : result._serverData.image_url,
+          in_stock : result._serverData.in_stock,
+          our_price : result._serverData.our_price,
+          rrp_price : result._serverData.rrp_price,
+          supplier : result._serverData.supplier,
+          discount : result._serverData.discount,
+          category : result._serverData.category
+        });
+      }));
+
+      $.mobile.hidePageLoadingMsg();
+      callback(Products);
+    },
+    error: function(error) {
+      forge.logging.info("Error: " + error.code + " " + error.message);
+    }
+  });
 }
 
 $(document).ready(function () {
--- js/views.js
+++ js/views.js
@@ -27,36 +27,11 @@ var ProductView = Backbone.View.extend({
 var AppView = Backbone.View.extend({
   el: $('body'),
   initialize: function() {
-
-    $.mobile.showPageLoadingMsg();
-
-    var query = new Parse.Query(Parse.Object.extend("Product"));
-    query.equalTo("supplier", "ChemistWarehouse");
-    query.find({
-      success: function(results) {
-        var Products = new ProductList(_.map(results, function(result) {
-          return new Product({
-            name : result._serverData.name,
-            image_url : result._serverData.image_url,
-            in_stock : result._serverData.in_stock,
-            our_price : result._serverData.our_price,
-            rrp_price : result._serverData.rrp_price,
-            supplier : result._serverData.supplier,
-            discount : result._serverData.discount,
-            category : result._serverData.category
-          });
-        }));
-
-        $.mobile.hidePageLoadingMsg();
-        Products.each(function(product){
-          var view = new ProductView({model: product});
-          this.$("#products-list").append(view.render().el);
-        }, this);
-      },
-      error: function(error) {
-        forge.logging.info("Error: " + error.code + " " + error.message);
-      }
-    });
-    
+    queryProducts(function(results){
+      results.each(function(product){
+        var view = new ProductView({model: product});
+        this.$("#products-list").append(view.render().el);
+      }, this);
+    });    
   }
 });
